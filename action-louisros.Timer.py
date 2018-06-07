02c368ee4d4cd29cd5cde62baaa7909b7ec363d8
Louis Ros <louis@ros.fr>
2018-06-07 15:20:52 +0200

    Initial commit

---
+++ action-louisros.Timer.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+import ConfigParser
+from hermes_python.hermes import Hermes
+from hermes_python.ontology import *
+import io
+
+CONFIGURATION_ENCODING_FORMAT = "utf-8"
+CONFIG_INI = "config.ini"
+
+class SnipsConfigParser(ConfigParser.SafeConfigParser):
+    def to_dict(self):
+        return {section : {option_name : option for option_name, option in self.items(section)} for section in self.sections()}
+
+
+def read_configuration_file(configuration_file):
+    try:
+        with io.open(configuration_file, encoding=CONFIGURATION_ENCODING_FORMAT) as f:
+            conf_parser = SnipsConfigParser()
+            conf_parser.readfp(f)
+            return conf_parser.to_dict()
+    except (IOError, ConfigParser.Error) as e:
+        return dict()
+
+def subscribe_intent_callback(hermes, intentMessage):
+    conf = read_configuration_file(CONFIG_INI)
+    action_wrapper(hermes, intentMessage, conf)
+
+
+def action_wrapper(hermes, intentMessage, conf):
+    """ Write the body of the function that will be executed once the intent is recognized. 
+    In your scope, you have the following objects : 
+    - intentMessage : an object that represents the recognized intent
+    - hermes : an object with methods to communicate with the MQTT bus following the hermes protocol. 
+    - conf : a dictionary that holds the skills parameters you defined 
+     
+    Refer to the documentation for further details. 
+    """ 
+    import time
+    import datetime
+    date = datetime.datetime.now()
+    lm = ['janvier','février','mars','avril','mai','juin',\
+    'juillet','aout','septembre','octobre','novembre','décembre']
+    ls = ['lundi','mardi','mercredi','jeudi','vendredi',\
+    'samedi','dimanche']
+    jour = str(date.day)
+    if date.day == 1 :
+        jour = "premier"
+    an = str(date.year)
+    mois = lm[date.month-1]
+    jsemaine = ls[date.weekday()]
+    resul = "nous sommes le " + jsemaine + " " + jour  + " " \
+    + mois 
+    current_session_id = intentMessage.session_id
+    hermes.publish_end_session(current_session_id, resul)
+    
+
+
+if __name__ == "__main__":
+    with Hermes("localhost:1883") as h:
+        h.subscribe_intent("louisros:date", subscribe_intent_callback) \
+         .start()
