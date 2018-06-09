abecbd9eac687087473a6d25a105e2c2f14fdb99
kheperV3 <louis@ros.fr>
2018-06-09 15:44:07 +0200

    Update action-louisros.Timer.py

--- action-louisros.Timer.py
+++ action-louisros.Timer.py
@@ -5,8 +5,10 @@ import ConfigParser
 from hermes_python.hermes import Hermes
 from hermes_python.ontology import *
 import io
+"""
 import paho.mqtt.client as mqtt
 import requests
+"""
 CONFIGURATION_ENCODING_FORMAT = "utf-8"
 CONFIG_INI = "config.ini"
 
