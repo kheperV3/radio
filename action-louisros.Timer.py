259204ec294ec89d2d6be71af47aa46954932cbb
kheperV3 <louis@ros.fr>
2018-06-10 17:15:21 +0200

    Update action-louisros.Timer.py

--- action-louisros.Timer.py
+++ action-louisros.Timer.py
@@ -6,9 +6,6 @@ from hermes_python.hermes import Hermes
 from hermes_python.ontology import *
 import io
 
-"""import paho.mqtt.client as mqtt
-import requests
-"""
 CONFIGURATION_ENCODING_FORMAT = "utf-8"
 CONFIG_INI = "config.ini"
 
@@ -32,23 +29,11 @@ def subscribe_intent_callback(hermes, intentMessage):
 
 
 def action_wrapper(hermes, intentMessage, conf):
-    """ Write the body of the function that will be executed once the intent is recognized. 
-    In your scope, you have the following objects : 
-    - intentMessage : an object that represents the recognized intent
-    - hermes : an object with methods to communicate with the MQTT bus following the hermes protocol. 
-    - conf : a dictionary that holds the skills parameters you defined 
-     
-    Refer to the documentation for further details. 
-   """
-    """delay = intentMessage.slots.valeur1.first().value
-    """
     current_session_id = intentMessage.session_id
-    """mqtt_client.publish('hermes/dialogueManager/endSession', json.dumps({'text':"coucou",'sessionId':current_session_id}))
-    """
     hermes.publish_end_session(current_session_id, "c'est fait Monsieur")
-    
-    
-    if __name__ == "__main__":
+
+
+if __name__ == "__main__":
     with Hermes("localhost:1883") as h:
         h.subscribe_intent("louisros:settimer", subscribe_intent_callback) \
-         .start()
+.start()
