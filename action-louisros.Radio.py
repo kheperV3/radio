d0c1bbb437c4fee3d19a013fc99ead687fe78958
kheperV3 <louis@ros.fr>
2018-07-04 18:43:33 +0200

    Update action-louisros.Radio.py

--- action-louisros.Radio.py
+++ action-louisros.Radio.py
@@ -54,11 +54,11 @@ def stop_callback(hermes, intentMessage):
 
 if __name__ == "__main__":
     with Hermes("localhost:1883") as h:
-        h.subscribe_intent("louisros:setStation", setStation_callback).start()
-        """
+        
+        
         h\
         .subscribe_intent("louisros:selectStation", selectStation_callback) \
         .subscribe_intent("louisros:changeVolume", volume_callback) \
         .subscribe_intent("louisros:stopRadio", stop_callback) \
         .start()
-        """
+        
