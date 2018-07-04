e6de455ba5340a6389290f477ed21729251f82a4
kheperV3 <louis@ros.fr>
2018-07-04 19:34:38 +0200

    Update action-louisros.Radio.py

--- action-louisros.Radio.py
+++ action-louisros.Radio.py
@@ -4,7 +4,7 @@
 import ConfigParser
 from hermes_python.hermes import Hermes
 from hermes_python.ontology import *
-#import io
+import io
 
 
 
@@ -17,39 +17,37 @@ class SnipsConfigParser(ConfigParser.SafeConfigParser):
  
 
 def read_configuration_file(configuration_file):
-    """try:
+    try:
         with io.open(configuration_file, encoding=CONFIGURATION_ENCODIN50G_FORMAT) as f:
             conf_parser = SnipsConfigParser()
             conf_parser.readfp(f)
             return conf_parser.to_dict()
     except (IOError, ConfigParser.Error) as e:
         return dict()
-"""
+
 
 
 def selectStation_callback(hermes, intentMessage):   
-#    conf = read_configuration_file(CONFIG_INI)   
+    conf = read_configuration_file(CONFIG_INI)   
     current_session_id = intentMessage.session_id
     hermes.publish_end_session(current_session_id, "station cher Maître")
     
 def volume_callback(hermes, intentMessage):
-   # conf = read_configuration_file(CONFIG_INI)   
+    conf = read_configuration_file(CONFIG_INI)   
     current_session_id = intentMessage.session_id
     hermes.publish_end_session(current_session_id, "volume cher Maître")
     
 
 def stop_callback(hermes, intentMessage):
-   # conf = read_configuration_file(CONFIG_INI)
+    conf = read_configuration_file(CONFIG_INI)
     current_session_id = intentMessage.session_id
     hermes.publish_end_session(current_session_id, "stop cher Maître")
 
 
 if __name__ == "__main__":
-    with Hermes("localhost:1883") as h:
+    with Hermes("localhost:1883") as h:    
         
-        
-        h\
-        .subscribe_intent("louisros:selectStation", selectStation_callback) \
+        h.subscribe_intent("louisros:selectStation", selectStation_callback) \
         .subscribe_intent("louisros:changeVolume", volume_callback) \
         .subscribe_intent("louisros:stopRadio", stop_callback) \
         .start()
