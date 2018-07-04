62199863c47b70395979c7923c3fbb02b24055a5
kheperV3 <louis@ros.fr>
2018-07-04 23:27:54 +0200

    Update action-louisros.Radio.py

--- action-louisros.Radio.py
+++ action-louisros.Radio.py
@@ -4,7 +4,7 @@
 import ConfigParser
 from hermes_python.hermes import Hermes
 from hermes_python.ontology import *
-import io
+#import io
 
 
 
@@ -16,7 +16,7 @@ class SnipsConfigParser(ConfigParser.SafeConfigParser):
         return {section : {option_name : option for option_name, option in self.items(section)} for section in self.sections()}
  
 
-def read_configuration_file(configuration_file):
+"""def read_configuration_file(configuration_file):
     try:
         with io.open(configuration_file, encoding=CONFIGURATION_ENCODIN50G_FORMAT) as f:
             conf_parser = SnipsConfigParser()
@@ -24,11 +24,14 @@ def read_configuration_file(configuration_file):
             return conf_parser.to_dict()
     except (IOError, ConfigParser.Error) as e:
         return dict()
-
+"""
 
 
 def callback(hermes, intentMessage):   
-    conf = read_configuration_file(CONFIG_INI)   
+#    conf = read_configuration_file(CONFIG_INI)   
+    
+    
+    
     current_session_id = intentMessage.session_id
     hermes.publish_end_session(current_session_id, "station cher Maître")
     
@@ -36,5 +39,5 @@ def callback(hermes, intentMessage):
 if __name__ == "__main__":
     with Hermes("localhost:1883") as h:    
         
-        h.subscribe_intents(callback).start()
+        h.subscribe_intents(self.callback).start()
         
