ee35e0b1b494dbcdb3fd0bd4b374cc8f8d44cc76
kheperV3 <louis@ros.fr>
2018-07-05 18:07:27 +0200

    Update action-louisros.radio.py

--- action-louisros.radio.py
+++ action-louisros.radio.py
@@ -15,7 +15,7 @@ MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 
 def intents_callback(hermes, intentMessage) : 
     
-    if intentMessage.intent.intent_name == 'selectStation' :
+    """if intentMessage.intent.intent_name == 'selectStation' :
         m = 'station'
     elif intentMessage.intent.intent_name == 'changeVolume' :
         m = 'volume'
@@ -23,7 +23,8 @@ def intents_callback(hermes, intentMessage) :
         m = 'radio'   
     else:
         m = 'je n'ai rien compris'
-    
+    """
+    m = "c'est difficile"
     current_session_id = intentMessage.session_id
     hermes.publish_end_session(current_session_id, m)
 
