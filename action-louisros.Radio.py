766976c41509f7e0d07457a09eb39f50c8def29e
kheperV3 <louis@ros.fr>
2018-07-05 11:28:31 +0200

    Update action-louisros.Radio.py

--- action-louisros.Radio.py
+++ action-louisros.Radio.py
@@ -47,5 +47,5 @@ def callback(self, hermes, intentMessage):
 if __name__ == "__main__":
     with Hermes("localhost:1883") as h:    
         
-        h.subscribe_intents(self.callback).start()
+        h.subscribe_intent('louisros:selectStation', self.callback).start()
         
