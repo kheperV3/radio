aa7af71436797424622c3c35afed5782d4558a45
kheperV3 <louis@ros.fr>
2018-07-05 17:52:17 +0200

    Update action-louisros.radio.py

--- action-louisros.radio.py
+++ action-louisros.radio.py
@@ -3,29 +3,17 @@
 
 import ConfigParser
 from hermes_python.hermes import Hermes
-from hermes_python.ontology import *
-import io
+
+MQTT_IP_ADDR = "localhost"
+MQTT_PORT = 1883
+MQTT_ADDR = "{}:{}".format(MQTT_IP_ADDR, str(MQTT_PORT))
 
 
 
-CONFIGURATION_ENCODING_FORMAT = "utf-8"
-CONFIG_INI = "config.ini"
 
-class SnipsConfigParser(ConfigParser.SafeConfigParser):
-    def to_dict(self):
-        return {section : {option_name : option for option_name, option in self.items(section)} for section in self.sections()}
- 
 
-def read_configuration_file(configuration_file):
-    try:
-        with io.open(configuration_file, encoding=CONFIGURATION_ENCODIN50G_FORMAT) as f:
-            conf_parser = SnipsConfigParser()
-            conf_parser.readfp(f)
-            return conf_parser.to_dict()
-    except (IOError, ConfigParser.Error) as e:
-        return dict()
 
-def action_wrapper(hermes,intentMessage,conf) :
+def intents_callback(hermes, intentMessage) : 
     
     """if intentMessage.intent.intent_name == 'selectStation' :
         m = 'station'
@@ -38,12 +26,6 @@ def action_wrapper(hermes,intentMessage,conf) :
     current_session_id = intentMessage.session_id
     hermes.publish_end_session(current_session_id, m)
 
-def intents_callback(hermes, intentMessage) : 
-    conf = read_configuration_file(CONFIG_INI)
-    action_wrapper(hermes,intentMessage,conf)
-        
- 
-    
 
 if __name__ == "__main__":
     with Hermes("localhost:1883") as h:    
