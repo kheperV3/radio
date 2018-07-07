b7a3ffa8fc626423dfff150f58b209bdcb77ab35
kheperV3 <louis@ros.fr>
2018-07-07 12:28:08 +0200

    Update action-louisros.radio.py

--- action-louisros.radio.py
+++ action-louisros.radio.py
@@ -39,13 +39,13 @@ def intents_callback(hermes, intentMessage) :
         live = fv.read()
         fv.close()
       
-        if live == 0 :
-            live = 1
-        if live == 4 :
-            live = 2
+        if live == "0000" :
+            live = "0001"
+        if live == "0004" :
+            live = "0002"
 
         fv =open("/var/lib/snips/skills/live","w") 
-        fv.write(str(live))
+        fv.write(live)
         fv.close()
 
         fv=open("/var/lib/snips/skills/link","w")
