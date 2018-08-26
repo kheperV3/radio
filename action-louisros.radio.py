6bf8c039ceff9d8d910681e10a660240bf1e9382
kheperV3 <louis@ros.fr>
2018-08-26 11:22:00 +0200

    Update action-louisros.radio.py

--- action-louisros.radio.py
+++ action-louisros.radio.py
@@ -196,7 +196,8 @@ def intents_callback(hermes, intentMessage) :
 if __name__ == "__main__":
     with Hermes(MQTT_ADDR) as h:           
         h.subscribe_intents(intents_callback)
-        h.loop_start()  
+        while 1:
+            sleep(0.1)
         
           
             
